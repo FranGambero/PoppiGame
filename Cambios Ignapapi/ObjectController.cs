7a18cee969803f57ede18f74bb24ce62fe032be8
Ignacio Aparicio Blanco <60527626+ignapa12@users.noreply.github.com>
2020-02-01 16:25:30 +0100

    Va en el objeto que cae
    Parametros:
    - zona en la que se tiene que recolocar

---
+++ Cambios Ignapapi/ObjectController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    private Quaternion originalRotation;
+    public Transform originalTransform;
+
+    private void Awake()
+    {
+        originalRotation = transform.rotation;
+    }
+    public Quaternion getOriginalRotation()
+    {
+        return originalRotation;
+    }
+
+    public Transform getOriginalTransform()
+    {
+        return originalTransform;
+    }
+}
